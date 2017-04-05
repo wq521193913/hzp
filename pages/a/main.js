dfef3a5f7cdaca3cd01f1dc51aa38b00f13d29b5
wq521193913 <521193913@qq.com>
2017-04-05 09:54:53 +0800

    commit 2017-04-05

---
+++ pages/a/main.js
@@ -0,0 +1,17 @@
+var app = getApp()
+Page({
+    data:{
+        adList:[
+            "/pages/resource/img/20170401152900.jpg",
+            "/pages/resource/img/20170401152932.jpg",
+            "/pages/resource/img/20170401152949.jpg",
+            "/pages/resource/img/20170401152957.jpg",
+            "/pages/resource/img/singledog.gif"
+        ]
+    },
+    onLoad:function(){
+        this.setData({
+
+        })
+    }
+})
